5b8d28fcfdae47c4331aabaff17cdff96a4c12a4
ashish960804 <ashishjha23415@gmail.com>
2021-10-04 12:13:35 +0530

    commit

--- bookshow/settings.py
+++ bookshow/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*273qmt2&u6k^d4&g9i1$qzcpvt971*963k$=rj-cu_q)0vp4-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','.movietoobook.herokuapp.com/']
 
